63c09b17202c6cbdf74a764bd2206dfd8c2b602f
muraliveesambattu <vmurali.ctr@gmail.com>
2026-07-28 08:01:47 +0530

    bootstrapped

---
+++ JS/JS9_CRUD_BOM/read.js
@@ -0,0 +1,8 @@
+function getUsersFromServer() {
+    //Get the Users from the Server
+    //Display That users in Table
+    //Add Edit And Delete Buttons in the Table
+    //when you click on Edit , navigate to edit.html page 
+    //when you click on Delete, delete the user and refresh the page
+    //Fill the data in the edit.html page , the the relavent users from server Example : http://localhost:3000/users/jpBLKpqlAfU
+}
